bf7a04f4872adc5ffdcd8be52fbcbf393fcd0941
Yuliia Vlasenko <47009437+another-priest@users.noreply.github.com>
2024-02-26 18:13:33 +0100

    metoda licz

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int licz(int a, int b)
+{
+    return a+b;
+}
+
+
+int main() {
+    printf("Hello, World!\n");
+    printf("W pierwszym branchu; 2+3= %d",licz(2,3));
+    return 0;
+}
